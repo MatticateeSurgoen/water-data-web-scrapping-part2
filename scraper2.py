512c8761b591d87fbe89619b3f20ec61abc5dfef
whoami <downloadme2002@protonmail.com>
2021-10-09 08:40:36 +0530

    commit

---
+++ scraper2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env	python3
+from selenium import webdriver
+import re
+from bs4 import BeautifulSoap
+import csv
+
+# checked contaminants and clicks show
+def show_Contaminants_separately(driver):
+	driver.execute_script("document.getElementById('ContentPlaceHolder_chkAll').checked = true;" +
+							"document.getElementById('ContentPlaceHolder_btnGO').click();")
+
+
+# Get webbrowser with options
+def get_webbrowser():
+	url = "https://ejalshakti.gov.in/IMISReports/Reports/WaterQuality/rpt_WQM_SampleTesting_S.aspx?Rep=0&RP=Y"
+	options = webdriver.ChromeOptions()
+	options.add_argument('--incognito')
+	#options.add_argument('--headless')		# to start in command line interface
+	driver = webdriver.Chrome(options=options)
+	driver.get(url)
+	return driver
+
+
+def get_length_from_id(driver, id_d):
+	length = int(driver.execute_script("return document.getElementById('" + id_d + "').length"))
+	return length
+
+def get_data(id_d, id_num):
+	return "document.getElementById('" + id_d + "').selectedIndex = " + str(id_num) + ';' + "document.getElementById('ContentPlaceHolder_btnGO').click();"
+
+
