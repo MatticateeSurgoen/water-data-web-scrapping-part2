83e30d492197bf6fc05ec3ae76f1611166191183
whoami <downloadme2002@protonmail.com>
2021-10-09 14:56:18 +0530

    commit

--- scraper2.py
+++ scraper2.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 import re
 from bs4 import BeautifulSoup
 import csv
+import numpy as np
 
 # checked contaminants and clicks show
 def show_Contaminants_separately(driver):
@@ -29,10 +30,15 @@ def get_data(id_d, id_num):
 	return "document.getElementById('" + id_d + "').selectedIndex = " + str(id_num) + ';' + "document.getElementById('ContentPlaceHolder_btnGO').click();"
 
 
-def get_tested_samples_by_id(id_d):
-	return f"document.getElementById('ContentPlaceHolder_rpt_lnkSamples_{id_d}').click()"
+def get_tested_samples_by_id(id_num):
+	return f"document.getElementById('ContentPlaceHolder_rpt_lnkSamples_{id_num}').click()"
 
 # cleaning up data for storing in csv format
 def clean_up(driver):
+	output = ''
 	soup = BeautifulSoup(driver.page_source, 'html.parser')
-	soup
+	for data in soup.select('table.SNewFontReportTable tr td'):
+		output += re.sub(' {2,}', '', BeautifulSoup.get_text(data)) + '|'
+	return output[:-1].split()
+
+
