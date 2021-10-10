b032b4f2070666e82c6ea8586be3563d9ea7cfd4
whoami <downloadme2002@protonmail.com>
2021-10-10 18:58:16 +0530

    commit

--- scraper2.py
+++ scraper2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env	python3
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,7 +11,7 @@ import numpy as np
 
 
 class SecretePage:
-	def __init__(driver):
+	def __init__(self, driver):
 		self.driver = driver
 
 	# checked contaminants and clicks show
@@ -19,10 +20,14 @@ class SecretePage:
 							"document.getElementById('ContentPlaceHolder_btnGO').click();")
 	def pages(self):
 		length = int(self.driver.execute_script("return document.getElementById('tableReportTable').children[1].childElementCount;"))
-		self.driver.execute_script(get_length_from_id())
+		# self.driver.execute_script(get_length_from_id())
 
-		#element = WebDriverWait(driver, 10).until(
-		#	 EC.element_to_be_clickable((By.ID, "myDynamicElement"))
+		for out in range(length - 1):
+			try:
+				element = WebDriverWait(self.driver, 2).until(
+					 EC.element_to_be_clickable((By.ID, f"ContentPlaceHolder_rpt_lnkSamples_{out}")))
+			except:
+				continue
  
 		
 	# cleaning up data for storing in csv format
@@ -31,8 +36,17 @@ class SecretePage:
 		soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 		for data in soup.select('table.SNewFontReportTable tr td'):
 			output += re.sub(' {2,}', '', BeautifulSoup.get_text(data)) + '|'
-		return output[:-1].split()
+		output = np.array(output[:-1].split('|'))
+		return output.reshape(len(output) // 33, 33)[:, 1:]
 
+	def next_page(self):
+		length = int(self.driver.execute_script("return document.getElementsByClassName('lnkPages').length"))
+		for data in range(length):
+			self.driver.execute_script(f"document.getElementById('ContentPlaceHolder_repIndex_lnkPages_{data}').click()")
+			yield self.clean_up()
+			time.sleep(4)
+
+	
 	def close(self):
 		self.driver.back()
 
@@ -88,21 +102,19 @@ def parser(driver, id_d):
 	for data in range(1, length):
 		driver.execute_script(get_data(id_d, data))
 		time.sleep(4)
+		yield 
 	
-
 def main():
 	# get webdriver
-	driver = webbrowser()
+	driver = get_webbrowser()
 	
 	for state in get_state_district(driver):
 		for district in get_district_block(driver):
 			for block in get_block_panchayat(driver):
 				for panchayat in get_panchayat_village(driver):
 					for village in get_village(driver):
+						pass
 	
-	
-
-
 
 if __name__ == '__main__':
 	main()
