9d5ab018b612971813765586c550ca0d2c7aa4be
k45ha1 <70165559+k45ha1@users.noreply.github.com>
2021-10-20 01:26:15 +0530

    Create install.sh

---
+++ collab/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install selenium
+apt-get update # to update ubuntu to correctly run apt install
+apt install chromium-chromedriver
+mv /usr/lib/chromium-browser/chromedriver /usr/bin
+
+git clone https://github.com/ml-project-pdpu/water-data
+
+cd water-data-web-scrapping-part2
