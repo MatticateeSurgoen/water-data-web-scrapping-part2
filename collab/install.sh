c7518331874221c5832b057e24aa2df36e956f09
k45ha1 <70165559+k45ha1@users.noreply.github.com>
2021-10-20 01:33:05 +0530

    Update install.sh

--- collab/install.sh
+++ collab/install.sh
@@ -4,7 +4,7 @@ apt-get update # to update ubuntu to correctly run apt install
 apt install chromium-chromedriver
 mv /usr/lib/chromium-browser/chromedriver /usr/bin
 
-git clone https://github.com/ml-project-pdpu/water-data
+git clone https://github.com/ml-project-pdpu/water-data-web-scrapping-part2
 
 cd water-data-web-scrapping-part2
 pip3 install -r requirements.txt
